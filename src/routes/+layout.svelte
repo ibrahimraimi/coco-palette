c271ac98940546e0f7c67c2b3ae1f5ce6a2b534d
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-06-01 23:56:02 +0100

    final

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
 	import "../app.pcss";
+
+	import Toast from "$lib/components/toast.svelte";
+	import { Toaster } from "$lib/components/ui/sonner";
+	import Metadata from "$lib/components/metadata.svelte";
 </script>
+
+<svelte:head>
+	<title>Coco Palette Generator</title>
+</svelte:head>
+
+<Metadata />
+<Toaster />
 
 <slot />
