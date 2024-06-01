abdba6642f4e7615e6fa5a6af02d15a030ce4762
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-06-01 23:55:03 +0100

    style: add ui components

---
+++ src/lib/components/minimal-button.svelte
@@ -0,0 +1,7 @@
+<button
+  on:click
+  class="opacity-50 transition-all hover:opacity-80 focus-visible:opacity-80 outline-none ring-black/5 focus-visible:ring-4 rounded-md disabled:pointer-events-none disabled:opacity-20"
+  {...$$restProps}
+>
+  <slot />
+</button>
